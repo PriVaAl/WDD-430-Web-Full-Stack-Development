168ea5a7989b05602feccb0d8428058d097e997b
PriVaAl <priscilla.valverde@gmail.com>
2026-05-19 22:39:11 -0600

    Update middleware

--- nextjs-dashboard/middleware.ts
+++ nextjs-dashboard/middleware.ts
@@ -1,7 +1,13 @@
-import NextAuth from 'next-auth';
-import { authConfig } from './auth.config';
+import { auth } from './auth';
 
-export default NextAuth(authConfig).auth;
+export default auth((req) => {
+  const isLoggedIn = !!req.auth;
+  const isOnDashboard = req.nextUrl.pathname.startsWith('/dashboard');
+  
+  if (isOnDashboard && !isLoggedIn) {
+    return Response.redirect(new URL('/login', req.nextUrl));
+  }
+});
 
 export const config = {
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
